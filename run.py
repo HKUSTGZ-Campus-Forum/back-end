10dd4a4d2aaedbbb454c7c996f94118be97d9096
Axfff <zhaojy2005@outlook.com>
2025-04-14 15:30:26 +0800

    improved CORS settings, also test github CD

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+CORS(app,
+     resources={r"/api/*": {"origins": ["https://dev.unikorn.axfff.com"]}},
+     supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,14 @@
 # wsgi.py
 from app import create_app
+from flask_cors import CORS
 
 application = create_app()
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app,
+     resources={r"/api/*": {"origins": ["https://unikorn.axfff.com", "https://www.unikorn.axfff.com"]}},
+     supports_credentials=True)
 
 if __name__ == '__main__':
     application.run()
