5304f3979f1fa59d82e7e0bbe43d02864190024d
Axfff <zhaojy2005@outlook.com>
2025-04-14 16:16:06 +0800

    updated CORS settings, test github action

--- run.py
+++ run.py
@@ -4,7 +4,13 @@ from flask_cors import CORS
 
 app = create_app()
 CORS(app,
-     resources={r"/api/*": {"origins": ["https://dev.unikorn.axfff.com"]}},
+     resources={r"/api/*":
+                    {"origins": [
+                        "https://dev.unikorn.axfff.com",
+                        "http://127.0.0.1:3000",
+                        "http://localhost:3000",
+                    ]
+                    }},
      supports_credentials=True)
 
 if __name__ == '__main__':
